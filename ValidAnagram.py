23ef6451dd11d1e86f128661eb7060cc5d66635f
Prudhvi Krishna Bodavula <pbodavul@asu.edu>
2023-02-22 19:34:36 -0700

    hi
    hi

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,14 +1,39 @@
 #TC: O(s+t) SC: O(s+t)
 def fun1(s,t):
-    if len(s)!=len(t):
-        return False
-    hm1,hm2={},{}
-    for i in range(len(s)):
-        hm1[s[i]]=1+hm1.get(s[i],0)
-        hm2[t[i]] = 1 + hm2.get(t[i], 0)
-    return hm1==hm2
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
 print(fun1("rat","car"))
 
 #Also try
 #TC : O(nlogn) SC:O(1)
 #return sorted(s)==sorted(t)
+
+
+def isAnagram(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    dic = {}
+    for i in s:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+
+    for j in t:
+        if j not in dic:
+            return False
+        else:
+            dic[j] -= 1
+
+    for val in dic.values():
+        if val != 0:
+            return False
+
+    return True
