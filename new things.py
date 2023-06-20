e707051fd8b9e01f0b9e3599b04af52ea61e16c3
Prudhvi Krishna Bodavula <pbodavul@asu.edu>
2023-06-19 18:33:27 -0700

    new

--- new things.py
+++ new things.py
@@ -1,24 +1,8 @@
-def lengthOfLongestSubstring(s):
-    n = len(s)
-    # Create a dictionary to store the last index of each character
-    last_index = {}
-    # Initialize variables to keep track of the start and end of the substring
-    start = 0
-    end = 0
-    # Initialize variable to keep track of the maximum length
-    max_length = 0
-    # Loop through the string
-    while end < n:
-        # Check if the current character has been seen before
-        if s[end] in last_index and last_index[s[end]] >= start:
-            # If the character has been seen, update the start of the substring
-            start = last_index[s[end]] + 1
-        # Update the last index of the current character
-        last_index[s[end]] = end
-        # Update the end of the substring
-        end += 1
-        # Update the maximum length
-        max_length = max(max_length, end - start)
-    return max_length
+a=[1,2]
+import copy
+b=a
+c=copy.deepcopy(b)
+a[0]=2
+print(a,b,c)
 
-print(lengthOfLongestSubstring("nndNfdfdf"))
+print((1,2)==(2,1))
