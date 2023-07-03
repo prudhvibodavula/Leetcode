9d5661606423e4e8d2e54114d0a968077c87e66e
Prudhvi Krishna Bodavula <pbodavul@asu.edu>
2023-07-03 16:15:08 -0700

    new

--- new things.py
+++ new things.py
@@ -1,8 +1,18 @@
-a=[1,2]
-import copy
-b=a
-c=copy.deepcopy(b)
-a[0]=2
-print(a,b,c)
+def sieve_of_eratosthenes(limit):
+    # Create a boolean array "is_prime[0..limit]" and initialize all entries as True
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime
 
-print((1,2)==(2,1))
+    # Mark multiples of all primes starting from 2
+    for num in range(2, int(limit**0.5) + 1):
+        if is_prime[num]:
+            for multiple in range(num**2, limit + 1, num):
+                is_prime[multiple] = False
+
+    # Collect all prime numbers into a list
+    primes = [num for num, is_prime in enumerate(is_prime) if is_prime]
+
+    return primes
+limit = 30
+primes = sieve_of_eratosthenes(limit)
+print(primes)
